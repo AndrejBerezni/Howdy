afb7db48b9d061a4cb427bb641341df5b160dfd2
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2024-05-16 20:58:34 +0100

    add animation to LoginPage and RegisterPage

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,33 @@
+import { motion } from 'framer-motion'
 import { Link } from 'react-router-dom'
 import LoginForm from '../components/LoginForm'
 import LogoWithName from '../components/LogoWithName'
 
 export default function LoginPage() {
   return (
-    <section className="flex flex-col items-center gap-8 py-8">
+    <motion.section
+      key="login-page"
+      initial={{
+        opacity: 0,
+        scaleY: 0,
+        filter: 'grayscale(100%) blur(10px)',
+      }}
+      animate={{
+        opacity: 1,
+        scaleY: 1,
+        filter: 'none',
+      }}
+      transition={{
+        duration: 1.5,
+        type: 'spring',
+        ease: 'linear',
+      }}
+      exit={{
+        x: -200,
+        opacity: 0,
+      }}
+      className="flex flex-col items-center gap-8 py-8"
+    >
       <LogoWithName />
       <LoginForm />
       <div className="w-full flex px-2 items-center text-lg md:text-xl text-primary uppercase gap-3 font-bold before:block before:h-[3px] before:flex-1 before:bg-primary after:block after:h-[3px] after:flex-1 after:bg-primary hover:cursor-default">
@@ -22,6 +45,6 @@ export default function LoginPage() {
           Sign up
         </Link>
       </p>
-    </section>
+    </motion.section>
   )
 }
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,11 +1,30 @@
+import { motion } from 'framer-motion'
 import LogoWithName from '../components/LogoWithName'
 import RegisterForm from '../components/RegisterForm'
 
 export default function RegisterPage() {
   return (
-    <section className="flex flex-col items-center gap-8 py-8">
+    <motion.section
+      key="register-page"
+      initial={{
+        opacity: 0,
+        x: 200,
+        filter: 'grayscale(100%) blur(10px)',
+      }}
+      animate={{
+        opacity: 1,
+        x: 0,
+        filter: 'none',
+      }}
+      transition={{
+        duration: 1.5,
+        type: 'spring',
+        ease: 'linear',
+      }}
+      className="flex flex-col items-center gap-8 py-8"
+    >
       <LogoWithName />
       <RegisterForm />
-    </section>
+    </motion.section>
   )
 }
