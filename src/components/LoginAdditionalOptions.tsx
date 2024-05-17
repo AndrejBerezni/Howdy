9346df0fcd6ec96c8624afd17873ff055faee44a
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2024-05-17 22:58:12 +0100

    create OAuthLoginButton component

--- src/components/LoginAdditionalOptions.tsx
+++ src/components/LoginAdditionalOptions.tsx
@@ -1,17 +1,43 @@
+import { useMemo } from 'react'
+import { FaDiscord } from 'react-icons/fa'
+import { FcGoogle } from 'react-icons/fc'
 import { Link } from 'react-router-dom'
+import OAuthLoginButton from './OAuthLoginButton'
 
 export default function LoginAdditionalOptions() {
+  const oAuthButtons = useMemo(
+    () => [
+      {
+        authMethod: 'Discord',
+        url: 'http://localhost:3000/api/v1/auth/discord/login',
+        icon: (
+          <FaDiscord className="md:text-3xl rounded-full bg-[#5865F2] group-hover:drop-shadow-[0_0_2px_#0A0606] group-hover:dark:drop-shadow-[0_0_2px_#F4EBEB] p-1 duration-150 text-white group-hover:scale-110" />
+        ),
+      },
+      {
+        authMethod: 'Google',
+        url: 'http://localhost:3000/api/v1/auth/google/login',
+        icon: (
+          <FcGoogle className="md:text-3xl group-hover:drop-shadow-[0_0_2px_#0A0606] group-hover:dark:drop-shadow-[0_0_2px_#F4EBEB] group-hover:scale-110 duration-150" />
+        ),
+      },
+    ],
+    []
+  )
+
   return (
     <>
       <div className="w-full flex px-2 items-center text-lg md:text-xl text-primary uppercase gap-3 font-bold before:block before:h-[3px] before:flex-1 before:bg-primary after:block after:h-[3px] after:flex-1 after:bg-primary hover:cursor-default">
         <p className="mx-6">or</p>
       </div>
-      <Link
-        to="http://localhost:3000/api/v1/auth/discord/login"
-        className="secondary-btn w-[calc(100%-1rem)] text-center"
-      >
-        Continue with Discord
-      </Link>
+      {oAuthButtons.map((btn) => (
+        <OAuthLoginButton
+          key={`${btn.authMethod}-oAuth-btn`}
+          authMethod={btn.authMethod}
+          url={btn.url}
+          icon={btn.icon}
+        />
+      ))}
       <p className="md:text-lg text-text dark:text-textDark">
         Don't have an account?
         <Link
---
+++ src/components/OAuthLoginButton.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+
+export default function OAuthLoginButton({
+  authMethod,
+  url,
+  icon,
+}: {
+  authMethod: string
+  url: string
+  icon: React.ReactNode
+}) {
+  return (
+    <Link
+      to={url}
+      className="group secondary-btn w-[calc(100%-1rem)] text-center flex items-center justify-center gap-4"
+    >
+      {icon}
+      Continue with {authMethod}
+    </Link>
+  )
+}
